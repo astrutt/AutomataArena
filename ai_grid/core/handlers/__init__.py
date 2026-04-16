c19ea7c940ab6faa4512881dc46ae56c2ab583a3
astrutt <andrew.strutt@gmail.com>
2026-04-15 23:58:48 -0400

    bug: export handle_stats in core handlers module

--- ai_grid/core/handlers/__init__.py
+++ ai_grid/core/handlers/__init__.py
@@ -9,6 +9,7 @@ from .personal import (
     handle_info_view,
     handle_tasks_view,
     handle_options,
+    handle_stats,
     handle_news_view
 )
 from .grid import (
@@ -50,7 +51,7 @@ from .admin import (
 
 __all__ = [
     'handle_help', 'is_machine_mode', 'check_rate_limit',
-    'handle_registration', 'handle_info_view', 'handle_tasks_view', 'handle_options', 'handle_news_view',
+    'handle_registration', 'handle_info_view', 'handle_tasks_view', 'handle_options', 'handle_stats', 'handle_news_view',
     'handle_grid_movement', 'handle_grid_view', 'handle_node_explore', 'handle_grid_map', 'handle_grid_command', 'handle_grid_loot', 'handle_grid_network_msg',
     'handle_shop_view', 'handle_merchant_tx', 'handle_auction', 'handle_market_view',
     'handle_powergen', 'handle_training', 'handle_gibson_status', 'handle_gibson_compile', 'handle_gibson_assemble', 'handle_item_use',
